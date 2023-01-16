095f0938ff913b0ff104f0715943c83bbb1b0cbf
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 18:23:19 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ContactsForm } from 'components/ContactsForm/ContactsForm';
+import { ContactsList } from 'components/ContactsList/ContactsList';
+import { Filter } from 'components/Filter/Filter';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactsForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactsList />
+      </div>
+    );
+  }
+}
