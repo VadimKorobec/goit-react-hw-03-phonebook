d6ea3fdee7aafae6a83f5fd413e28ae18c0ff88b
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 19:38:58 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,7 +2,7 @@ export const Filter = ({ filter, onChange }) => {
   return (
     <label>
       Find contacts by name
-      <input type="text" name="filter" filter={filter} onChange={onChange} />
+      <input type="text" name="filter" value={filter} onChange={onChange} />
     </label>
   );
 };
