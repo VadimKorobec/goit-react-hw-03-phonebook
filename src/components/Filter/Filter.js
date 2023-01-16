f812f3a1d17701511132db51f0dfb80bb17faa17
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 19:20:34 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,3 +1,12 @@
-export const Filter = () => {
-  return <input />;
+export const Filter = ({ filter, onChange }) => {
+  return (
+    <label>
+      <input
+        type="text"
+        name={filter}
+        onChange={onChange}
+        placeholder="Find contacts by name"
+      />
+    </label>
+  );
 };
