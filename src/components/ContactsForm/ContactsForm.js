d81aab0cb2344cbbe75a77f34deb2436b6cae96e
VadimKorobec <_s_e_t__@ukr.net>
2023-01-16 19:20:39 +0200

    Update ContactsForm.js

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -5,14 +5,33 @@ export class ContactsForm extends Component {
     name: '',
     number: '',
   };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget.value;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
   render() {
+    const { name, number } = this.state;
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
           Name
           <input
             type="text"
             name="name"
+            value={name}
+            onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -23,6 +42,8 @@ export class ContactsForm extends Component {
           <input
             type="tel"
             name="number"
+            value={number}
+            onChange={this.handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
