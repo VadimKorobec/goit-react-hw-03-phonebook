8ea30c13a576118ee04b52065b5e2f1e750921f5
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 15:10:50 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,19 @@ import { Component } from 'react';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
   };
 
   addContact = (name, number) => {
     console.log(name, number);
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { name, number }],
+    }));
   };
 
   render() {
@@ -17,7 +25,7 @@ export class App extends Component {
         <h1>Phonebook</h1>
         <ContactsForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
-        <ContactsList items={this.state.contacts} />
+        <ContactsList contacts={this.state.contacts} />
       </div>
     );
   }
