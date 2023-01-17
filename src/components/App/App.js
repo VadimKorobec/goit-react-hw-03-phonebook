c6218a5892694aa058f7e15d02f93579ba165df4
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 13:54:02 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,15 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+  };
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
@@ -16,6 +25,8 @@ export class App extends Component {
             <input
               type="text"
               name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
@@ -26,6 +37,8 @@ export class App extends Component {
             <input
               type="tel"
               name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
