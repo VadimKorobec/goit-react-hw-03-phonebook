d03d6d61ecda897f896f2bc6b9007f6f8ada5df2
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 18:16:38 +0200

    Update ContactsItem.js

--- src/components/ContactsItem/ContactsItem.js
+++ src/components/ContactsItem/ContactsItem.js
@@ -1,12 +1,12 @@
 export const ContactItem = ({ contact: { id, name, number }, onDelete }) => {
   return (
-    <div>
+    <li key={id}>
       <p>
         {name}:{number}
       </p>
       <button type="button" onClick={() => onDelete(id)}>
         Delete
       </button>
-    </div>
+    </li>
   );
 };
