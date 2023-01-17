bf80f88db993ac7a8001f787034041b27b918fb0
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 18:16:40 +0200

    Update ContactsForm.js

--- src/components/ContactsForm/ContactsForm.js
+++ src/components/ContactsForm/ContactsForm.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Component } from 'react';
 
 export class ContactsForm extends Component {
@@ -8,16 +9,12 @@ export class ContactsForm extends Component {
 
   handleChange = event => {
     const { name, value } = event.currentTarget;
-
-    this.setState({
-      [name]: value,
-    });
+    this.setState({ [name]: value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    const { name, number } = event.currentTarget.elements;
-    this.props.onSubmit(name.value, number.value);
+    this.props.onSubmit(this.state);
     this.reset();
   };
 
@@ -25,27 +22,33 @@ export class ContactsForm extends Component {
     this.setState({ name: '', number: '' });
   };
 
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+
   render() {
+    const { name, number } = this.state;
+
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <label htmlFor={this.nameId}>
           Name
           <input
             type="text"
             name="name"
-            value={this.state.name}
+            value={name}
             onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
         </label>
-        <label>
+        <label htmlFor={this.numberId}>
           Number
           <input
             type="tel"
             name="number"
-            value={this.state.number}
+            value={number}
             onChange={this.handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
