926dde00f07e955d2386a89bca046808ebfff623
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 17:14:35 +0200

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,7 +2,7 @@ export const Filter = () => {
   return (
     <label>
       Find contacts by name
-      <input type="text" />
+      <input type="text" value={filter} onChange />
     </label>
   );
 };
