d92202998a6a2994ac505dfc776dc39bcecb10f2
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 14:58:27 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import { ContactsForm } from 'components/ContactsForm/ContactsForm';
+import { ContactsList } from 'components/ContactsList/ContactsList';
 import { Component } from 'react';
 
 export class App extends Component {
@@ -6,16 +7,17 @@ export class App extends Component {
     contacts: [],
   };
 
-  formSubmitHandle = data => {
-    console.log(data);
+  addContact = (name, number) => {
+    console.log(name, number);
   };
 
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactsForm onSubmit={this.formSubmitHandle} />
+        <ContactsForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
+        <ContactsList items={this.state.contacts} />
       </div>
     );
   }
