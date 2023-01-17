b66857bd0959ae545cad23d86554df9a3f736937
VadimKorobec <_s_e_t__@ukr.net>
2023-01-17 12:54:24 +0200

    Update ContactsList.js

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,11 +1,3 @@
-import { ContactItem } from 'components/ContactsItem/ContactsItem';
-
-export const ContactsList = ({ contacts, onDelete }) => {
-  return (
-    <ul>
-      {contacts.map(({ id, name, number }) => (
-        <ContactItem id={id} name={name} number={number} onDelete={onDelete} />
-      ))}
-    </ul>
-  );
+export const ContactsList = () => {
+  return <ul></ul>;
 };
